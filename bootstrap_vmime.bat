44e35d8679a18b413498cc9cbea8a6a024d9c2ea
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2015-10-13 12:05:01 +0200

    augh

--- bootstrap_vmime.bat
+++ bootstrap_vmime.bat
@@ -7,6 +7,7 @@ mkdir vmime-build
 cd vmime-build
 cmake -G "Visual Studio 14 2015 Win64"^
  -DVMIME_BUILD_SHARED_LIBRARY=OFF^
+ -DVMIME_BUILD_STATIC_LIBRARY=ON^
  -DVMIME_HAVE_SASL_SUPPORT=OFF^
  -DVMIME_HAVE_TLS_SUPPORT=OFF^
  -DVMIME_HAVE_MESSAGING_PROTO_SENDMAIL=OFF^
@@ -15,7 +16,7 @@ cmake -G "Visual Studio 14 2015 Win64"^
  -DVMIME_CHARSETCONV_LIB=icu^
  -DICU_ROOT=%ICU_ROOT%^
  ..\vmime
-cmake --build . --config Release
+cmake --build . --config Release --target INSTALL
 cd ..
 
 
--- httpcv.cpp
+++ httpcv.cpp
@@ -1,354 +1,170 @@
-#include <array>
-#include <string>
+#include "Simple-Web-Server/server_http.hpp"
+#include "Simple-Web-Server/client_http.hpp"
 
-#include <stdlib.h>
+#include "vmime/vmime.hpp"
 
-#include "opencv2/core.hpp"
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
-#include <boost/thread.hpp>
-#include <boost/lexical_cast.hpp>
+//Added for the json-example
+#define BOOST_SPIRIT_THREADSAFE
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
 
-#define WINVER 0x0500
-#include <windows.h>
+//Added for the default_resource example
+#include <fstream>
+#include <boost/filesystem.hpp>
 
-#define NUM_BOUND_KEYS 3
+using namespace std;
+//Added for the json-example:
+using namespace boost::property_tree;
 
-const char g_szClassName[] = "greenshift";
+typedef SimpleWeb::Server<SimpleWeb::HTTP> HttpServer;
+typedef SimpleWeb::Client<SimpleWeb::HTTP> HttpClient;
 
-struct boundKey {
-    int id;
-    std::string text;
-    int width;
-    int height;
-    WORD virtualKey;
-};
+int main() {
+    //HTTP-server at port 8080 using 4 threads
+    HttpServer server(8080, 4);
     
-struct boundKey boundKeys[NUM_BOUND_KEYS] = {
-  {1, "Shift", 40, 18, VK_SHIFT},
-  {2, "Ctrl", 40, 18, VK_CONTROL},
-  {3, "Alt", 40, 18, VK_MENU}
-};
-
-boundKey *getById(int id) {
-  boundKey *key;
-  for (int i=0; i<NUM_BOUND_KEYS; i++) {
-    if (boundKeys[i].id == id) {
-      key=&boundKeys[i];
-      break;
-    }
-  }
-  return key;
-}
-
-void sendBoundKey(WORD virtualKey, bool down) {
-  
-    INPUT ip;
-    
-    // Set up a generic keyboard event.
-    ip.type = INPUT_KEYBOARD;
-    ip.ki.wScan = 0; // hardware scan code for key
-    ip.ki.time = 0;
-    ip.ki.dwExtraInfo = 0;
- 
-    // Press the key
-    ip.ki.wVk = virtualKey; // virtual-key code for the key
-    ip.ki.dwFlags = 0; // 0 for key press
-    if (!down) {
-      ip.ki.dwFlags = KEYEVENTF_KEYUP;
-    }
-    SendInput(1, &ip, sizeof(INPUT));
-}
-
-boundKey *selectedKey = NULL;
-boundKey *activeKey = NULL;
-
-HBRUSH hbrBkgndD = NULL;
-HBRUSH hbrBkgndS = NULL;
-HBRUSH hbrBkgndA = NULL;
-
-
-void greenDetect(HWND hwnd) {
-    int fontFace = cv::FONT_HERSHEY_SCRIPT_SIMPLEX;
-    double fontScale = 1;
-    int thickness = 1;  
-    
-    cv::Point textOrg(500, 25);
-    
-    cv::Mat frame, hsv, thresholded;
-    
-    int numWhite = 0;
-    
-    int iLowH = 75;
-    int iHighH = 104;
-    /*
-    int iLowH = 32;
-    int iHighH = 51;
-    */        
-    int iLowS = 29; 
-    int iHighS = 142;
-    
-    int iLowV = 38;
-    int iHighV = 255;
-    
-    cv::namedWindow("Control", cv::WINDOW_AUTOSIZE); //create a window called "Control"
-    
-    //Create trackbars in "Control" window
-    cv::createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
-    cv::createTrackbar("HighH", "Control", &iHighH, 179);
-    
-    cv::createTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-    cv::createTrackbar("HighS", "Control", &iHighS, 255);
-    
-    cv::createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
-    cv::createTrackbar("HighV", "Control", &iHighV, 255);
-    
-    cv::VideoCapture cap(0); // open the default camera
-    
-    if(cap.isOpened()) {
-    
-      cv::namedWindow("thresholded", 1);
-      cv::waitKey(1000); // Wait for camera so frame won't be empty
-    
-      for (;;)
-      {
+    //Add resources using path-regex and method-string, and an anonymous function
+    //POST-example for the path /string, responds the posted string
+    server.resource["^/string$"]["POST"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        //Retrieve string:
+        auto content=request->content.string();
+        //request->content.string() is a convenience function for:
+        //stringstream ss;
+        //ss << request->content.rdbuf();
+        //string content=ss.str();
         
-          try
-          {
-              boost::this_thread::interruption_point();
-          }
-          catch(boost::thread_interrupted&)
-          {
-              break;
-          }
+        response << "HTTP/1.1 200 OK\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+    };
+    
+    //POST-example for the path /json, responds firstName+" "+lastName from the posted json
+    //Responds with an appropriate error message if the posted json is not valid, or if firstName or lastName is missing
+    //Example posted json:
+    //{
+    //  "firstName": "John",
+    //  "lastName": "Smith",
+    //  "age": 25
+    //}
+    server.resource["^/json$"]["POST"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        try {
+            ptree pt;
+            read_json(request->content, pt);
 
-          cap >> frame; // get a new frame from capture
-          cv::cvtColor(frame, hsv, cv::COLOR_BGR2HSV);
-          
-          cv::inRange(hsv, cv::Scalar(iLowH, iLowS, iLowV), cv::Scalar(iHighH, iHighS, iHighV), thresholded); //Threshold the image
-          
-          //morphological opening (remove small objects from the foreground)
-          cv::erode(thresholded, thresholded, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)) );
-          cv::dilate(thresholded, thresholded, cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)) ); 
-          
-          //morphological closing (fill small holes in the foreground)
-          cv::dilate(thresholded, thresholded, getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)) ); 
-          cv::erode(thresholded, thresholded, getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(5, 5)) );
-          
-          numWhite = cv::countNonZero(thresholded);
-            
-          cv::putText(thresholded,
-            boost::lexical_cast<std::string>(numWhite),
-            textOrg, fontFace, fontScale, cv::Scalar::all(255), thickness, 8);
-          
-          if (numWhite > 10000 ) {
-            if (activeKey == NULL && selectedKey != NULL) {
-              activeKey = selectedKey;
-              sendBoundKey(activeKey->virtualKey, TRUE); 
-              InvalidateRect(hwnd, 0, TRUE);
-            }
-          } else {
-            if (activeKey != NULL) {
-              sendBoundKey(activeKey->virtualKey, FALSE);
-              activeKey = NULL; 
-              InvalidateRect(hwnd, 0, TRUE);
+            string name=pt.get<string>("firstName")+" "+pt.get<string>("lastName");
 
-            }
-          }
-            
-            
-          cv::imshow("thresholded", thresholded);
-          
-          cv::waitKey(30);
-          
+            response << "HTTP/1.1 200 OK\r\nContent-Length: " << name.length() << "\r\n\r\n" << name;
         }
-    }
-}
-
-// Step 4: the Window Procedure
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch(msg)
-    {
-
-        case WM_CLOSE:
-            DestroyWindow(hwnd);
-        break;
-        case WM_DESTROY:
-            DeleteObject(hbrBkgndD);
-            DeleteObject(hbrBkgndS);
-            DeleteObject(hbrBkgndA);
-            PostQuitMessage(0);
-        case WM_CREATE:
-        {
-            int left = 0;
-            for (int i=0; i<NUM_BOUND_KEYS; i++) {
-              HFONT hfDefault;
-              HWND hStatic;
-              hStatic = CreateWindowEx(WS_EX_CLIENTEDGE, "Static", boundKeys[i].text.c_str(), 
-                  WS_CHILD | WS_VISIBLE | SS_CENTER | SS_NOTIFY, 
-                  0, 0, boundKeys[i].width, boundKeys[i].height, hwnd, (HMENU)boundKeys[i].id, GetModuleHandle(NULL), NULL);
-              if(hStatic == NULL)
-                  MessageBox(hwnd, "Could not create edit box.", "Error", MB_OK | MB_ICONERROR);
-
-              hfDefault = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
-              SendMessage(hStatic, WM_SETFONT, (WPARAM)hfDefault, MAKELPARAM(FALSE, 0));
-              SetWindowPos(hStatic, 0, left, 0, 0, 0, SWP_NOZORDER | SWP_NOSIZE );
-              left = left + boundKeys[i].width + 5;
-            }
+        catch(exception& e) {
+            response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << strlen(e.what()) << "\r\n\r\n" << e.what();
         }
-        break;
-        case WM_COMMAND:
-        {
-            switch (HIWORD(wParam))
-            {
-              case STN_CLICKED:
-              {
-                
-                int id = (int)LOWORD(wParam);
-                boundKey *tempSelectedKey = getById(id);
-                if (selectedKey != NULL && activeKey == NULL) {
-                  if (selectedKey == tempSelectedKey) selectedKey = NULL;
-                  else selectedKey = tempSelectedKey;
+    };
+    
+    //GET-example for the path /info
+    //Responds with request-information
+    server.resource["^/info$"]["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        stringstream content_stream;
+        content_stream << "<h1>Request from " << request->remote_endpoint_address << " (" << request->remote_endpoint_port << ")</h1>";
+        content_stream << request->method << " " << request->path << " HTTP/" << request->http_version << "<br>";
+        for(auto& header: request->header) {
+            content_stream << header.first << ": " << header.second << "<br>";
+        }
+        
+        //find length of content_stream (length received using content_stream.tellp())
+        content_stream.seekp(0, ios::end);
+        
+        response <<  "HTTP/1.1 200 OK\r\nContent-Length: " << content_stream.tellp() << "\r\n\r\n" << content_stream.rdbuf();
+    };
+    
+    //GET-example for the path /match/[number], responds with the matched string in path (number)
+    //For instance a request GET /match/123 will receive: 123
+    server.resource["^/match/([0-9]+)$"]["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        string number=request->path_match[1];
+        response << "HTTP/1.1 200 OK\r\nContent-Length: " << number.length() << "\r\n\r\n" << number;
+    };
+    
+    //Default GET-example. If no other matches, this anonymous function will be called. 
+    //Will respond with content in the web/-directory, and its subdirectories.
+    //Default file: index.html
+    //Can for instance be used to retrieve an HTML 5 client that uses REST-resources on this server
+    server.default_resource["GET"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        boost::filesystem::path web_root_path("web");
+        if(!boost::filesystem::exists(web_root_path))
+            cerr << "Could not find web root." << endl;
+        else {
+            auto path=web_root_path;
+            path+=request->path;
+            if(boost::filesystem::exists(path)) {
+                if(boost::filesystem::canonical(web_root_path)<=boost::filesystem::canonical(path)) {
+                    if(boost::filesystem::is_directory(path))
+                        path+="/index.html";
+                    if(boost::filesystem::exists(path) && boost::filesystem::is_regular_file(path)) {
+                        ifstream ifs;
+                        ifs.open(path.string(), ifstream::in | ios::binary);
+                        
+                        if(ifs) {
+                            ifs.seekg(0, ios::end);
+                            size_t length=ifs.tellg();
+                            
+                            ifs.seekg(0, ios::beg);
+                            
+                            response << "HTTP/1.1 200 OK\r\nContent-Length: " << length << "\r\n\r\n";
+                            
+                            //read and send 128 KB at a time
+                            size_t buffer_size=131072;
+                            vector<char> buffer;
+                            buffer.reserve(buffer_size);
+                            size_t read_length;
+                            try {
+                                while((read_length=ifs.read(&buffer[0], buffer_size).gcount())>0) {
+                                    response.write(&buffer[0], read_length);
+                                    response.flush();
+                                }
+                            }
+                            catch(const exception &e) {
+                                cerr << "Connection interrupted, closing file" << endl;
+                            }
+
+                            ifs.close();
+                            return;
+                        }
+                    }
                 }
-                else if (selectedKey == NULL) selectedKey = tempSelectedKey;
-
-                InvalidateRect(hwnd, 0, TRUE);
-              }
             }
         }
-        break;
-        case WM_CTLCOLORSTATIC:
-        {
-          if ( activeKey != NULL && (int)GetDlgCtrlID((HWND)lParam) == activeKey->id) {
-            HDC hdcStatic = (HDC) wParam;
-            SetTextColor(hdcStatic, RGB(0,0,0));
-            SetBkColor(hdcStatic, RGB(70,255,70));
-              
-            if (hbrBkgndA == NULL)
-            {
-                hbrBkgndA = CreateSolidBrush(RGB(70,255,70));
-            }
-          return (INT_PTR)hbrBkgndA; 
-          } else if ( selectedKey != NULL && (int)GetDlgCtrlID((HWND)lParam) == selectedKey->id) {
-            HDC hdcStatic = (HDC) wParam;
-            SetTextColor(hdcStatic, RGB(255,255,255));
-            SetBkColor(hdcStatic, RGB(70,70,255));
-              
-            if (hbrBkgndS == NULL)
-            {
-                hbrBkgndS = CreateSolidBrush(RGB(70,70,255));
-            }
-          return (INT_PTR)hbrBkgndS; 
-          } else {
-            HDC hdcStatic = (HDC) wParam;
-            SetTextColor(hdcStatic, RGB(0,0,0));
-            SetBkColor(hdcStatic, RGB(255,255,255));
-              
-            if (hbrBkgndD == NULL)
-            {
-              hbrBkgndD = CreateSolidBrush(RGB(255,255,255));
-            }
-            return (INT_PTR)hbrBkgndD;
-          }
-        }
-        break;
-        default:
-            return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
-}
+        string content="Could not open path "+request->path;
+        response << "HTTP/1.1 400 Bad Request\r\nContent-Length: " << content.length() << "\r\n\r\n" << content;
+    };
+    server.default_resource["POST"]=[](HttpServer::Response& response, shared_ptr<HttpServer::Request> request) {
+        vmime::utility::inputStreamAdapter is(request->content);
+        vmime::string data;
+        vmime::utility::outputStreamStringAdapter os(data);
+        vmime::utility::bufferedStreamCopy(is , os);
+        vmime::shared_ptr<vmime::message> msg = vmime::make_shared<vmime::message>();
+        const auto it=request->header.find("Content-Type");
+        msg->parse(data);       
 
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
+        vmime::messageParser mp(msg);/*
+// Output information about attachments
+cout << "Message has " << mp.getAttachmentCount() << "attachment(s)" << endl;
+for(int i=0;i<mp.getAttachmentCount();++i)
 {
-    WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Message;
-    Message.message = ~WM_QUIT;
+    vmime::shared_ptr <const vmime::attachment> att = mp.getAttachmentAt(i);
+    cerr << " − " << att->getType().generate() << endl;
+}*/
     
-    //Step 1: Registering the Window Class
-    wc.cbSize        = sizeof(WNDCLASSEX);
-    wc.style         = 0;
-    wc.lpfnWndProc   = WndProc;
-    wc.cbClsExtra    = 0;
-    wc.cbWndExtra    = 0;
-    wc.hInstance     = hInstance;
-    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
-    wc.lpszMenuName  = NULL;
-    wc.lpszClassName = g_szClassName;
-    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
+        string name="me";
+        response << "HTTP/1.1 200 OK\r\nContent-Length: " << name.length() << "\r\n\r\n" << name;
+                
+    };
+        
+    thread server_thread([&server](){
+        //Start server
+        server.start();
+    });
+    
+    //Wait for server to start so that the client can connect
+    this_thread::sleep_for(chrono::seconds(1));
 
-    if(!RegisterClassEx(&wc))
-    {
-        MessageBox(NULL, "Window Registration Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    // Step 2: Creating the Window
-    hwnd = CreateWindowEx(
-        WS_EX_TOPMOST,
-        g_szClassName,
-        "Greenshift",
-        WS_POPUP ,
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 60,
-        NULL, NULL, hInstance, NULL);
-
-    if(hwnd == NULL)
-    {
-        MessageBox(NULL, "Window Creation Failed!", "Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
+    server_thread.join();
     
-    RECT rc;
-    
-    GetWindowRect ( hwnd, &rc ) ;
-    
-    int xPos = (GetSystemMetrics(SM_CXSCREEN) - rc.right)-100;
-    // int yPos = (GetSystemMetrics(SM_CYSCREEN) - rc.bottom)+20;
-    int yPos = 2;
-
-    SetWindowPos( hwnd, 0, xPos, yPos, 0, 0, SWP_NOZORDER | SWP_NOSIZE );
-    
-    int left = 0;
-    HRGN visibleRgn = CreateRectRgn(0,0,0,0);
-    
-    for (int i=0; i<NUM_BOUND_KEYS; i++) {
-        HRGN tempRgn = CreateRectRgn(
-          left,
-          0,
-          left+boundKeys[i].width,
-          boundKeys[i].height
-        );
-        left = left + boundKeys[i].width + 5;
-        CombineRgn(visibleRgn, visibleRgn, tempRgn, RGN_OR);
-        DeleteObject(tempRgn);
-    }
-    
-    SetWindowRgn(hwnd, visibleRgn, FALSE);
-    ShowWindow(hwnd, nCmdShow);
-    UpdateWindow(hwnd);
-    
-    boost::thread greenDetectThread(&greenDetect, hwnd);    
-
-    while (Message.message != WM_QUIT)
-    {
-        if (PeekMessage(&Message, NULL, 0, 0, PM_REMOVE))
-        {
-            // If a message was waiting in the message queue, process it
-            TranslateMessage(&Message);
-            DispatchMessage(&Message);
-        }
-
-    }
-    
-    greenDetectThread.interrupt();
-    greenDetectThread.join();
-    
-    return Message.wParam;
+    return 0;
 }
