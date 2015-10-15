e02bffaa09403a50862c35a03b8a47b591212fb5
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2015-10-15 12:37:04 +0200

    apply cleanly and readd opencv

--- configure.bat
+++ configure.bat
@@ -10,9 +10,19 @@ set "VMIME_ROOT=%PREFIXDRIVE%\vmime"
 
 if "%1" equ "bootstrap" (
     
+    git clone https://github.com/Itseez/opencv.git opencv_sources
+    mkdir opencv_build
+    cd opencv_build
+    cmake -G "Visual Studio 14 2015 Win64"^
+     -DBUILD_SHARED_LIBS=OFF^
+     -DCMAKE_INSTALL_PREFIX=%OPENCV_DIR%^
+     ..\opencv_sources
+    cmake --build . --config Release --target INSTALL
+    cd ..
+
     git clone http://github.com/kisli/vmime.git
     cd vmime
-    git apply ..\vmime_msvc_mt.diff
+    git apply --reject --whitespace=fix ..\vmime_msvc_mt.diff
     cd ..
     mkdir vmime-build
     cd vmime-build
